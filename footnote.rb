24cec8878fa40ddf9f4a617c17bed99166ebf827
Felipe Cypriano <felipe@cypriano.me>
2011-12-02 16:22:59 -0200

    Implement footnote tags
    Usage in footnote.rb header
    
    tags:
    fn_ref
    footnote_ref
    
    footnotes
      - fn

--- footnote.rb
+++ footnote.rb
@@ -6,7 +6,7 @@
 #
 # Usage:
 #
-# This text needs to point to a footnote{% fn_ref 1 %} because it's good{% fn_ref 2 %}.
+# This text needs to point to a footnote{% fn_ref 1 %} because it's good{% footnote_ref 2 %}.
 #
 # {% footnotes %}
 #   {% fn A footnote is this text at the end of you page that explains something. %}
@@ -15,25 +15,66 @@
 #
 # HTML generated
 #
-# This text needs to point to a footnote<sup id="fnref:1"><a href="#fn:1">1</a></sup> because it's good<sup id="fnref:2"><a href="#fn:2">2</a></sup>.
+# This text needs to point to a footnote<sup id="fnref:1"><a href="#fn:1" rel="footnote">1</a></sup> because it's good<sup id="fnref:2"><a href="#fn:2" rel="footnote">2</a></sup>.
 #
 # <div class="footnotes">
 # 	<ol>
-# 		<li id="fn:1">A footnote is this text at the end of you page that explains something.<a href="#fnref:1">↩</a></li>
-# 		<li id="fn:2">It's good because I like it.<a href="#fnref:2">↩</a></li>
+# 		<li id="fn:1">A footnote is this text at the end of you page that explains something.<a href="#fnref:1" rev="footnote">↩</a></li>
+# 		<li id="fn:2">It's good because I like it.<a href="#fnref:2" rev="footnote">↩</a></li>
 # 	</ol>
 # </div>
 #
 
 
-module Jekyll
+module Footnote
 
-	class FootnoteRefTag < Liquid::Tag
-		def initialize(tag_name, markup, tokens)
+	class ReferenceTag < Liquid::Tag
+		def initialize(tag_name, ref, tokens)
+			super
+			@reference = ref.strip unless ref.nil?
 		end
 
 		def render(context)
+			unless @reference.nil?
+				"<sup id='fnref:#{@reference}'><a href='#fn:#{@reference}' rel='footnote'>#{@reference}</a></sup>"
+			end
 		end
 	end
 
+	class FootnotesBlock < Liquid::Block
+		def initialize(tag_name, markup, tokens)
+			@footnotes = []
+			@current_reference = 1
+			super
+		end
+
+		def render(context)
+			<<-end_html
+<div class="footnotes">
+	<ol>
+		#{@footnotes.join}
+	</ol>
+</div>
+			end_html
+		end
+
+		def unknown_tag(name, params, tokens)
+			if name == 'fn'
+				handle_fn_tag(params)
+			else
+				super
+			end
+		end
+
+		private
+		def handle_fn_tag(params)
+			ref_text = params
+			@footnotes << "<li id='fn:#{@current_reference}'>#{ref_text}<a href='#fnref:#{@current_reference}' rev='footnote'>↩</a></li>"
+			@current_reference += 1
+		end
+	end
 end
+
+Liquid::Template.register_tag('footnote_ref', Footnote::ReferenceTag)
+Liquid::Template.register_tag('fn_ref', Footnote::ReferenceTag)
+Liquid::Template.register_tag('footnotes', Footnote::FootnotesBlock)
